01fb3323b085ce3405a27a5a29152e61c712f2f0
Samuel Horwitz <samuel.horwitz@gmail.com>
2019-07-08 17:30:46 -0400

    more updates

--- web/assets/recordcrate.js
+++ web/assets/recordcrate.js
@@ -12,7 +12,7 @@ export async function initialize(countryCode) {
         return;
     }
     initializeCalled = true;
-    let response = await fetch(`${process.env.API_ORIGIN}/spotify/tracks?token=${await getAccessToken()}`);
+    let response = await fetch(`${process.env.API_ORIGIN}/spotify/tracks?token=${await getAccessToken()}`, {mode: 'no-cors'});
     let encodedTracks = await response.arrayBuffer();
     let recordCrateWorker = new RecordCrateWorker();
     await new Promise((resolve, reject) => {
