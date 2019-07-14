878d466f1c25f2e899c8fe2e7ce73489df798969
Samuel Horwitz <samuel.horwitz@gmail.com>
2019-07-14 16:33:09 -0400

    postgres connection polishing

--- web/api/config.go
+++ web/api/config.go
@@ -1,15 +1,18 @@
 package main
 
 type config struct {
-	isProduction             bool
-	phosphorOrigin           string
-	spotifyClientID          string
-	spotifySecret            string
-	spacesID                 string
-	spacesSecret             string
-	spacesTracksEndpoint     string
-	spacesTracksRegion       string
-	spacesScriptsEndpoint    string
-	spacesScriptsRegion      string
-	postgresConnectionString string
+	isProduction                         bool
+	phosphorOrigin                       string
+	spotifyClientID                      string
+	spotifySecret                        string
+	spacesID                             string
+	spacesSecret                         string
+	spacesTracksEndpoint                 string
+	spacesTracksRegion                   string
+	spacesScriptsEndpoint                string
+	spacesScriptsRegion                  string
+	postgresConnectionString             string
+	postgresMaxOpenConnections           int
+	postgresMaxIdleConnections           int
+	postgresMaxConnectionLifetimeMinutes int
 }
