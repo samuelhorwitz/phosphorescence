7adbca2dab353c9d9bc3052db5b7cab8d024973b
Samuel Horwitz <samuel.horwitz@gmail.com>
2019-09-13 08:40:05 -0400

    fix logout

--- web/api/session/cookies.go
+++ web/api/session/cookies.go
@@ -43,31 +43,21 @@ func setCookies(w http.ResponseWriter, sessionID string, refreshID string, perma
 
 func clearCookies(w http.ResponseWriter) {
 	sessionCookie := &http.Cookie{
-		Name:     SessionCookieName,
-		Value:    "",
-		Expires:  time.Unix(0, 0),
-		MaxAge:   -1,
-		Domain:   cookieDomain,
-		Path:     "/",
-		Secure:   true,
-		HttpOnly: true,
-	}
-	if isProduction {
-		sessionCookie.SameSite = http.SameSiteLaxMode
+		Name:    SessionCookieName,
+		Value:   "",
+		Domain:  cookieDomain,
+		Path:    "/",
+		Expires: time.Unix(0, 0),
+		MaxAge:  -1,
 	}
 	http.SetCookie(w, sessionCookie)
 	refreshCookie := &http.Cookie{
-		Name:     RefreshCookieName,
-		Value:    "",
-		Expires:  time.Unix(0, 0),
-		MaxAge:   -1,
-		Domain:   cookieDomain,
-		Path:     "/",
-		Secure:   true,
-		HttpOnly: true,
-	}
-	if isProduction {
-		refreshCookie.SameSite = http.SameSiteLaxMode
+		Name:    RefreshCookieName,
+		Value:   "",
+		Domain:  cookieDomain,
+		Path:    "/",
+		Expires: time.Unix(0, 0),
+		MaxAge:  -1,
 	}
 	http.SetCookie(w, refreshCookie)
 }
